92abcce0fb8fa7764ef08ef1d18d1cb612ae3e1f
Eric Mugo <mugoericpg20@gmail.com>
2023-08-03 10:49:29 +0300

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,11 @@ module.exports = {
     content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {},
+  variants: {},
   plugins: [],
+  
 }
 
